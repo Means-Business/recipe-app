e83370049a7cd718da22615459f4e79e808c19f6
ManotLuijiu <manot_luijiu@hotmail.com>
2019-04-26 16:16:44 +0700

    add api

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import './App.css';
 
 import Form from './components/Form';
 
+const API_KEY = '44d744111e17df1ceb1928f02c515224';
+
 class App extends React.Component {
-  getRecipe = (e) => {
+  getRecipe = async e => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    console.log(recipeName);
-  }
+    const api_call = await fetch(
+      `https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=5`
+    );
+
+    const data = await api_call.json();
+    console.log(data);
+  };
   render() {
     return (
       <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Recipe Search</h1>
-      </header>
-      <Form getRecipe={this.getRecipe} />
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe} />
       </div>
-    )
+    );
   }
 }
 
